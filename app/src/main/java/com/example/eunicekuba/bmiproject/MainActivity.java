571878902a6556ec95ad2b8b3569fe6983712d7a
Ricardo Sgobbe <sgobbericardo@yahoo.com.br>
2016-04-10 17:08:37 -0300

    Implementação webview, falta melhorias na apresentaçao do resultado

--- app/src/main/java/com/example/eunicekuba/bmiproject/MainActivity.java
+++ app/src/main/java/com/example/eunicekuba/bmiproject/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.eunicekuba.bmiproject.fragment.ProgressFragment;
+import com.example.eunicekuba.bmiproject.fragment.WebFragment;
 
 /**
  * Created by eu.nicekuba on 09/04/2016.
@@ -30,9 +31,11 @@ public class MainActivity extends AppCompatActivity {
     private ObjectAnimator mParentAnimation;
     private RelativeLayout mInputWeight;
     private Button mButtonCalculate;
+    private Button mButtonWeb;
     private FrameLayout mFrameContainer;
     private FragmentTransaction mFragmentTransaction;
     private ProgressFragment mProgFrag;
+    private WebFragment mWebFragment;
     private EditText mWeight;
     private EditText mHeight;
     private boolean mIsFieldsOk;
@@ -47,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         mWeight = (EditText) findViewById(R.id.id_text_weight);
         mHeight = (EditText) findViewById(R.id.id_text_height);
         configButton();
+        configButtonWeb();
         configFragContainer();
         setSupportActionBar(mToolbar);
 
@@ -66,26 +70,45 @@ public class MainActivity extends AppCompatActivity {
                 checkFields();
                 if(mIsFieldsOk){
                     configParentAnimation();
+                    FragmentManager manager = getSupportFragmentManager();
+                    mFragmentTransaction = manager.beginTransaction();
+                    mProgFrag = new ProgressFragment();
+                    mFragmentTransaction.replace(R.id.id_frag_container, mProgFrag);
+                    mFragmentTransaction.addToBackStack(null);
+                    mFragmentTransaction.commit();
                     mParentAnimation.start();
+
                 }
+            }
+        });
+    }
+
+    private void configButtonWeb(){
+        mButtonWeb = (Button) findViewById(R.id.id_btn_web);
+        mButtonWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                configParentAnimation();
+                FragmentManager manager = getSupportFragmentManager();
+                mFragmentTransaction = manager.beginTransaction();
+                mWebFragment = new WebFragment();
+                mFragmentTransaction.replace(R.id.id_frag_container, mWebFragment);
+                mFragmentTransaction.addToBackStack(null);
+                mFragmentTransaction.commit();
+                mParentAnimation.start();
             }
         });
     }
 
     private void configParentAnimation() {
         mParentLayout = (RelativeLayout) findViewById(R.id.id_relative_child);
-        mParentAnimation = ObjectAnimator.ofFloat(mParentLayout, "y", mInputWeight.getHeight(), mToolbar.getHeight()- mParentLayout.getHeight());
+        mParentAnimation = ObjectAnimator.ofFloat(mParentLayout, "y", mInputWeight.getHeight(), mToolbar.getHeight() - mParentLayout.getHeight());
         mParentAnimation.setInterpolator(new BounceInterpolator());
         mParentAnimation.setDuration(1000);
         mFrameContainer.setVisibility(View.VISIBLE);
-        FragmentManager manager = getSupportFragmentManager();
-        mFragmentTransaction = manager.beginTransaction();
-        mProgFrag = new ProgressFragment();
-        mFragmentTransaction.replace(R.id.id_frag_container, mProgFrag);
-        mFragmentTransaction.addToBackStack(null);
-        mFragmentTransaction.commit();
-
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -95,7 +118,11 @@ public class MainActivity extends AppCompatActivity {
         mParentAnimation.setDuration(1000);
         mParentAnimation.start();
         mFrameContainer.setVisibility(View.INVISIBLE);
-        mFragmentTransaction.remove(mProgFrag);
+        if(mWebFragment == null){
+            mFragmentTransaction.remove(mProgFrag);
+        }else{
+            mFragmentTransaction.remove(mWebFragment);
+        }
 
     }
 
@@ -112,17 +139,6 @@ public class MainActivity extends AppCompatActivity {
             mHeight.setError("Height in centimeter !");
             mIsFieldsOk = false;
         }
-
-        if(height.contains("-")){
-            mHeight.setError("Number not valid");
-            mIsFieldsOk = false;
-        }
-
-        if(weight.contains("-")){
-            mWeight.setError("Number not valid");
-            mIsFieldsOk = false;
-        }
-
         if(weight.isEmpty()){
             mWeight.setError("Empty field !");
             mIsFieldsOk = false;
---
+++ app/src/main/java/com/example/eunicekuba/bmiproject/fragment/WebFragment.java
@@ -0,0 +1,46 @@
+package com.example.eunicekuba.bmiproject.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.eunicekuba.bmiproject.R;
+
+/**
+ * Created by eu.nicekuba on 10/04/2016.
+ */
+public class WebFragment extends Fragment {
+
+    private WebView mWebView;
+    private static final String mURL = "https://en.wikipedia.org/wiki/Body_mass_index";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.layout_fragment_web, container, false);
+        mWebView = (WebView) v.findViewById(R.id.id_webview);
+        configWebView();
+        return v;
+    }
+
+    private void configWebView() {
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+        mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+        mWebView.loadUrl(mURL);
+    }
+}
